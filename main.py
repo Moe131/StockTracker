a191dc598fa93f349d19403de0442b9a5dabe10b
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2023-12-22 00:43:08 -0800

    The program now fetches stocks prices in the watchlist and prints the watchlist

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 # main.py
 
+from stock import Stock
+from watchlist import Watchlist
+
 def main():
-    pass
+
+    mywatch = Watchlist('main')
+    mywatch.add(Stock('AAPL'))
+    mywatch.add(Stock('NIO'))
+    mywatch.add(Stock('TSLA'))
+    mywatch.add(Stock('SPY'))
+    mywatch.printWatchlist()
 
 if __name__ == '__main__':
     main()
+
+
+# Sample Output
+#
+##        Stock       Price       Change
+#---------------------------------------
+#1        AAPL        194.68      -0.08
+#2        NIO          8.28       4.67
+#3        TSLA        254.50       2.98
+#4        SPY        472.80       0.95
